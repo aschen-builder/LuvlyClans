a9b2b8ead32a3811a0bd6b5630af64870faa352f
aschen-builder <auschick.work@gmail.com>
2021-07-12 18:47:28 -0700

    null handle shipcontroll

--- LuvlyClans/Patches/ShipControllsPatches.cs
+++ LuvlyClans/Patches/ShipControllsPatches.cs
@@ -5,15 +5,30 @@ namespace LuvlyClans.Patches
     [HarmonyPatch]
     public static class ShipControllsPatches
     {
-        [HarmonyPatch(typeof(Door), "Interact")]
+        [HarmonyPatch(typeof(ShipControlls), "Interact")]
         [HarmonyPrefix]
         public static bool ShipControllsInteract(bool __result, ShipControlls __instance, Humanoid character)
         {
             long characterOwner = character.GetOwner();
-            long shipOwner = __instance.GetShip().m_nview.GetZDO().m_owner;
+
+            Ship ship = __instance.GetShip();
+
+            if (ship is null)
+            {
+                return true;
+            }
+
+            long shipOwner = ship.m_nview.GetZDO().m_owner;
 
             Player characterPlayer = Utils.GetPlayerFromOwner(characterOwner);
             Player shipPlayer = Utils.GetPlayerFromOwner(shipOwner);
+
+            bool isPlayerNull = characterPlayer is null || shipPlayer is null;
+
+            if (isPlayerNull)
+            {
+                return true;
+            }
 
             TribeManager.TribeManager tm = new TribeManager.TribeManager(shipPlayer, characterPlayer);
 
