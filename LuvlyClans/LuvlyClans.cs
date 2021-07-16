ef824352cc740722c719eafc98a57943dfa8643b
aschen-builder <auschick.work@gmail.com>
2021-07-16 00:30:08 -0700

    nuke it all

--- LuvlyClans/LuvlyClans.cs
+++ LuvlyClans/LuvlyClans.cs
@@ -7,10 +7,6 @@
 using BepInEx;
 using HarmonyLib;
 using Log = Jotunn.Logger;
-using JSON = SimpleJson.SimpleJson;
-using LuvlyClans.Patches;
-using LuvlyClans.Types.Clans;
-using System.IO;
 
 namespace LuvlyClans
 {
@@ -27,86 +23,15 @@ namespace LuvlyClans
         public const string ClansPath = "luvly.clans.json";
         public const string BackupClansPath = "luvly.clans.old.json";
 
-        public static Clans AllClans;
-
         private readonly Harmony harmony = new Harmony(PluginGUID);
 
         private void Awake()
         {
-           Log.LogInfo("LuvlyClans Initialized");
-            LoadClansDB();
-            LoadPatches();
+            return;
         }
 
         private void Update()
         {
-            //WriteToClansDB();
-
-            return;
-        }
-
-        private void LoadClansDB()
-        {
-            string path = Path.Combine(ConfigPath, ClansPath);
-
-            if (File.Exists(path))
-            {
-                Log.LogMessage("Found Clans DB");
-                string file = File.ReadAllText(path);
-
-                AllClans = JSON.DeserializeObject<Clans>(file);
-                Log.LogInfo($"Loaded {AllClans.m_clans.Length} clans");
-                Log.LogInfo(AllClans.m_clans[0].m_members[0].m_playerID);
-
-                return;
-            }
-
-            Log.LogMessage("No Clans DB Found");
-        }
-
-        private void LoadPatches()
-        {
-            Log.LogInfo("Patching Clan Permissions");
-
-            harmony.PatchAll(typeof(ContainerPatches));
-            harmony.PatchAll(typeof(CharacterPatches));
-        }
-
-        public void BackupClansDB()
-        {
-            if (File.Exists(Path.Combine(ConfigPath, ClansPath)))
-            {
-                if (File.Exists(Path.Combine(ConfigPath, BackupClansPath)))
-                {
-                    File.Delete(Path.Combine(ConfigPath, BackupClansPath));
-                }
-
-                File.Copy(Path.Combine(ConfigPath, ClansPath), Path.Combine(ConfigPath, BackupClansPath));
-
-                Log.LogMessage("Clans DB Backed Up Successfully");
-
-                return;
-            }
-        }
-
-        private void WriteToClansDB()
-        {
-            /** 
-             * create global clans update array to then merge with AllClans
-             */
-            Log.LogMessage("Updateing Clans DB");
-
-            BackupClansDB();
-
-            string file = JSON.SerializeObject(AllClans);
-
-            if (file != null)
-            {
-                File.WriteAllText(Path.Combine(ConfigPath, ClansPath), file);
-            }
-
-            Log.LogMessage("Successfully Updated Clans DB");
-
             return;
         }
     }
