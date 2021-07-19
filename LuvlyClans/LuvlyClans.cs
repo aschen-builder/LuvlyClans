cbcf0d8dccc68026869618f2599cd34f732b29b8
aschen-builder <auschick.work@gmail.com>
2021-07-18 21:18:55 -0700

    refactor main plugin add clan helpers

--- LuvlyClans/LuvlyClans.cs
+++ LuvlyClans/LuvlyClans.cs
@@ -6,33 +6,57 @@
 
 using BepInEx;
 using HarmonyLib;
+using Jotunn;
+using LuvlyClans.Patches;
+using LuvlyClans.Server.Types;
+using LuvlyClans.Server;
 using Log = Jotunn.Logger;
+using JSON = SimpleJson.SimpleJson;
+using Jotunn.Utils;
 
 namespace LuvlyClans
 {
   [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-  [BepInDependency(Jotunn.Main.ModGuid)]
-  //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+  [BepInDependency(Main.ModGuid)]
+  [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
   internal class LuvlyClans : BaseUnityPlugin
   {
         public const string PluginGUID = "com.aschenbuilder.luvlyclans";
         public const string PluginName = "LuvlyClans";
         public const string PluginVersion = "0.0.1";
 
-        public static string ConfigPath = Paths.ConfigPath;
-        public const string ClansDBPath = "luvly.clans.json";
-        public const string BackupClansDBPath = "luvly.clans.old.json";
+        public static string m_path_config = BepInEx.Paths.ConfigPath;
+        public const string m_path_db = "luvly.clans.json";
+        public const string m_path_db_backup = "luvly.clans.old.json";
+
+        public static bool m_is_server;
+        public static bool m_is_client;
+
+        public static DB m_db_server;
+        public static Clans m_clans_server;
+        public static Clans m_clans_client;
+        public static Clans m_clans_db;
 
         private readonly Harmony harmony = new Harmony(PluginGUID);
 
         private void Awake()
         {
-            return;
+            Log.LogInfo("Loading patches");
+            harmony.PatchAll(typeof(GamePatches));
+            harmony.PatchAll(typeof(ZNetPatches));
+            harmony.PatchAll(typeof(DoorPatches));
+            harmony.PatchAll(typeof(ContainerPatches));
+            harmony.PatchAll(typeof(MinimapPatches));
         }
 
         private void Update()
         {
             return;
+        }
+
+        public static string GetServerClansJSON()
+        {
+            return JSON.SerializeObject(m_clans_server);
         }
     }
 }
